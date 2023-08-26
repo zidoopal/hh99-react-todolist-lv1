bf776954e9c2a757ac7609218c1c998014799602
zidoopal <yuzzzin@gmail.com>
2023-08-27 00:43:03 +0900

    TodoWorking 컴포넌트 생성

---
+++ src/components/TodoWorking.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoItem from './TodoItem.jsx';
+
+function TodoListWorking() {
+  return (
+    <div>
+      <h2>Working...🔥</h2>
+      <div>
+        <TodoItem />
+      </div>
+    </div>
+  );
+}
+
+export default TodoListWorking;
