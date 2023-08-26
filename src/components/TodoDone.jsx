2960c1a5c2ec7e29592e1f692c0f683978b78328
zidoopal <yuzzzin@gmail.com>
2023-08-27 00:44:12 +0900

    TodoDone 컴포넌트 생성

---
+++ src/components/TodoDone.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoItem from './TodoItem.jsx';
+
+function TodoDone() {
+  return (
+    <div>
+      <h2>Done ! ✨</h2>
+      <div>
+        <TodoItem />
+      </div>
+    </div>
+  );
+}
+
+export default TodoDone;
